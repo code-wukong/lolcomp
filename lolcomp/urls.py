9fc1507275e7978691c3aac648373072efae5501
mmserran <MarkAnthony.Serrano2@gmail.com>
2015-04-08 00:01:00 -0700

    added(django_app): sitedown page

--- lolcomp/urls.py
+++ lolcomp/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'lolcomp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('sitedown.urls')),
+    url(r'^.*$', RedirectView.as_view(url='/', permanent=False), name='index'),
 ]
